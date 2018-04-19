d8a663d817db85d3f4f9fccb1552423f9fec8ff5
Gábor Koródi <gabor_korodi@hms.harvard.edu>
2018-04-19 10:53:33 -0400

    Fix case sensitivity for table names

--- resource/i2b2/delete.sql
+++ resource/i2b2/delete.sql
@@ -1,18 +1,19 @@
 set @resourceName = '<RESOURCE_NAME>';
 SET FOREIGN_KEY_CHECKS = 0;
 
-DELETE FROM `PredicateType_dataTypes` WHERE PredicateType_id IN (SELECT supportedPredicates_id FROM resource_predicatetype WHERE resource_id IN (SELECT id FROM resource WHERE NAME = @resourceName));
-DELETE FROM predicatetype_field WHERE predicatetype_id IN (SELECT supportedPredicates_id FROM resource_predicatetype WHERE resource_id IN (SELECT id FROM resource WHERE name = @resourceName));
-DELETE FROM predicatetype_paths WHERE predicatetype_id IN (SELECT supportedPredicates_id FROM resource_predicatetype WHERE resource_id IN (SELECT id FROM resource WHERE name = @resourceName));
-DELETE FROM predicatetype WHERE id IN (SELECT supportedPredicates_id FROM resource_predicatetype WHERE resource_id IN (SELECT id FROM resource WHERE name = @resourceName));
+DELETE FROM `PredicateType_dataTypes` WHERE PredicateType_id IN (SELECT supportedPredicates_id FROM `Resource_PredicateType` WHERE resource_id IN (SELECT id FROM `Resource` WHERE NAME = @resourceName));
+DELETE FROM `PredicateType_Field` WHERE predicatetype_id IN (SELECT supportedPredicates_id FROM `Resource_PredicateType` WHERE resource_id IN (SELECT id FROM `Resource` WHERE name = @resourceName));
+DELETE FROM `PredicateType_paths` WHERE predicatetype_id IN (SELECT supportedPredicates_id FROM `Resource_PredicateType` WHERE resource_id IN (SELECT id FROM `Resource` WHERE name = @resourceName));
+DELETE FROM `PredicateType` WHERE id IN (SELECT supportedPredicates_id FROM `Resource_PredicateType` WHERE resource_id IN (SELECT id FROM `Resource` WHERE name = @resourceName));
 
-DELETE FROM resource_predicatetype WHERE resource_id IN (SELECT id FROM resource WHERE  NAME = @resourceName);  
-DELETE FROM resource_field WHERE resource_id IN (SELECT id FROM resource WHERE  NAME = @resourceName);
-DELETE FROM resource_relationships WHERE resource_id IN (SELECT id FROM resource WHERE  NAME = @resourceName);                                      
-DELETE FROM resource_logicaloperator WHERE id IN (SELECT id FROM   resource WHERE  NAME = @resourceName); 
-DELETE FROM resource_datatypes WHERE resource_id IN (SELECT id FROM   resource WHERE  NAME = @resourceName); 
-DELETE FROM resource_parameters WHERE id IN (SELECT id FROM resource WHERE  NAME = @resourceName);
+DELETE FROM `Resource_PredicateType` WHERE resource_id IN (SELECT id FROM `Resource` WHERE  NAME = @resourceName);  
+DELETE FROM `Resource_Field` WHERE resource_id IN (SELECT id FROM `Resource` WHERE  NAME = @resourceName);
+DELETE FROM `Resource_relationships` WHERE resource_id IN (SELECT id FROM `Resource` WHERE  NAME = @resourceName);                                      
+DELETE FROM `Resource_LogicalOperator` WHERE id IN (SELECT id FROM `Resource` WHERE  NAME = @resourceName); 
+DELETE FROM `Resource_dataTypes` WHERE resource_id IN (SELECT id FROM `Resource` WHERE  NAME = @resourceName); 
+DELETE FROM `resource_parameters` WHERE id IN (SELECT id FROM `Resource` WHERE  NAME = @resourceName);
 
-DELETE FROM resource WHERE name = @resourceName;
+DELETE FROM `Resource` WHERE name = @resourceName;
 
 SET FOREIGN_KEY_CHECKS = 1;
+commit;
